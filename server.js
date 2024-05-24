0b42070495f4b1780508f89953a772ee91681179
“pardhu <pardhuvuppala890@gmail.com>
2024-05-25 00:37:15 +0530

    Added

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ app.use(bodyParser.json());
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-    cors({
-        origin:["http://localhost:3000","https://Container-Management-App.onrender.com"]
-    })
-)
+
 
 const my_mongoose = require('./db');
 const userApi = require('./controllers/user_controller_api')
