2801f4eca670ed3a284021c5dea80015cfcf6b56
“pardhu <pardhuvuppala890@gmail.com>
2024-05-25 09:32:11 +0530

    Commented

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
 
 
 const my_mongoose = require('./db');
