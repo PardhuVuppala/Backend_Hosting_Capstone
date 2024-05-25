2e422ddcc4cc89d44bcdea859269c762d76a6196
PardhuVuppala <106818434+PardhuVuppala@users.noreply.github.com>
2024-05-25 11:02:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ app.use('/Notification',NotificationApi)
 app.use('/payment',PaymentApi)
 
 
-app.listen(4500, () => console.log('EXPRESS Server Started at Port No: 4500 '));
+app.listen(1200, () => console.log('EXPRESS Server Started at Port No: 1200 '));
