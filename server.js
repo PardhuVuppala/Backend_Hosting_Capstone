54034eb968fef3d20cd2e73670c4e9df551b4cf6
“pardhu <pardhuvuppala890@gmail.com>
2024-05-25 09:43:39 +0530

    Commented

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors())
-app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
 
+//app.use(express.urlencoded({ extended: false }));
+app.use(cors(
+    {
+        origin : ["https://container-management-website.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true   
+    }
+))
+app.use(express.json());
 
 const my_mongoose = require('./db');
 const userApi = require('./controllers/user_controller_api')
